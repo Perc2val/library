e16c9d81c2f0507300d20233d02760f500042f97
Perc2val <fsb099@aol.de>
2026-05-09 23:10:52 +0200

    displayBooks properly done

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 const body = document.querySelector("body")
+const bookList = document.querySelector(".bookList")
 
 const myLibrary = [];
 
@@ -21,21 +22,22 @@ function addBookToLibrary(title, author, pages, read) {
 };
 
 function displayLibrary(){
-    for (const elements of myLibrary){
+    bookList.innerHTML = "";
+    myLibrary.forEach(element => {
         let shell = document.createElement("div");
-        body.appendChild(shell);
+        bookList.appendChild(shell);
         shell.classList.add("shell")
         let heading = document.createElement("h3");
-        heading.textContent = `Title: ${elements.title}`;
+        heading.textContent = `Title: ${element.title}`;
         shell.appendChild(heading);
         let showAuthor = document.createElement("p");
-        showAuthor.textContent = `Author: ${elements.author}`;
+        showAuthor.textContent = `Author: ${element.author}`;
         shell.appendChild(showAuthor);
         let showPages = document.createElement("p");
-        showPages.textContent = `Pages: ${elements.pages}`;
+        showPages.textContent = `Pages: ${element.pages}`;
         shell.appendChild(showPages);
         let showRead = document.createElement("p");
-        showRead.textContent = `Read: ${elements.read}`;
+        showRead.textContent = `Read: ${element.read}`;
         shell.appendChild(showRead);
-    }
+    })
 };
